a72cb4182eeb8bc688d2c47c1cc0f90330b0f332
jongrover <hello@jonathangrover.com>
2016-03-30 15:44:12 -0400

    solve async-io

--- my-first-async-io/program.js
+++ my-first-async-io/program.js
@@ -2,5 +2,7 @@ var fs = require('fs'),
     file = process.argv[2];
 
 fs.readFile(file, 'utf-8', function(err, data){
-  
+  var array = data.split('\n'),
+      numLines = array.length - 1;
+  console.log(numLines);
 });
